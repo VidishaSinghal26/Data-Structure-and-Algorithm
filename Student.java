966216456af49fa4feb3b2fdb04480ff671368b7
VidishaSinghal26 <90644367+VidishaSinghal26@users.noreply.github.com>
2022-01-11 17:33:03 +0530

    Print the student detail how got max and min marks

---
+++ Student.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class Student {
+    private int rollno;
+    private int marks;
+    private String name;
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void display(String name ,int rollno,int marks) {
+        System.out.println(name + " " + rollno + " " + marks);
+    }
+
+    public static void main(String[] args) {
+        Student arr[] = new Student[10];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            arr[i] = new Student();
+            arr[i].setRollno(sc.nextInt());
+            arr[i].setMarks(sc.nextInt());
+            arr[i].setName(sc.next());
+        }
+        int max = arr[0].marks;
+        int min = arr[0].marks;
+        for (int i = 0; i < 10; i++) {
+            if (arr[i].marks > max) {
+                max = arr[i].marks;
+            } else if (arr[i].marks < min) {
+                min = arr[i].marks;
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            if (arr[i].marks == max) {
+                arr[i].marks=0;
+                //arr[i].display(arr[i].name,arr[i].rollno,arr[i].marks);
+                //System.out.println(arr[i].name+" "+arr[i].rollno+" "+arr[i].marks);
+            }
+
+//            else if (arr[i].marks == min)
+//            {
+//                arr[i].display(arr[i].name,arr[i].rollno,arr[i].marks);
+//               // System.out.println(arr[i].name + " " + arr[i].rollno + " " + arr[i].marks);
+//            }
+        }
+        max=arr[0].marks;
+        for (int i = 0; i < 10; i++) {
+            if (arr[i].marks > max) {
+                max = arr[i].marks;
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            if (arr[i].marks == max) {
+              //  arr[i].marks = 0;
+                arr[i].display(arr[i].name,arr[i].rollno,arr[i].marks);
+            }
+        }
+
+    }
+
+}
