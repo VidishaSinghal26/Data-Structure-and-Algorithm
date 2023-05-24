3686b20bdaf94e29dd3616ce293317077c7e72c9
Vidisha Singhal <90644367+VidishaSinghal26@users.noreply.github.com>
2023-05-24 17:21:56 +0530

    Update Shift.java

--- Array/Shift.java
+++ Array/Shift.java
@@ -1,3 +1,16 @@
+// One array is given 
+// You have to find all the elements which are greater than all of their right side elements.
+// Array can be sorted or unsorted.
+// Tc - O(n)
+// Sc- 0(1)
+
+// Ex- 7, 10, 4, 3, 6, 5, 2
+// O/P:-10,6,5,2
+// Here you can see that 10,6,5,2 is greater than all of its right elements.
+
+// I/P:-10,20,30
+// O/P:-30
+
 import java.util.*;
 
 public class Shift {
